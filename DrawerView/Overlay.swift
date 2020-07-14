dd3b052368f215c33a59e594cff1610fd873783d
Nikolay Sotskiy <sotskiy@gmail.com>
2020-07-14 16:06:38 +0300

    Remove overlay mask

--- DrawerView/Overlay.swift
+++ DrawerView/Overlay.swift
@@ -10,8 +10,6 @@ import UIKit
 
 class Overlay: UIView {
 
-    private var _mask = CAShapeLayer()
-
     public var cornerRadius: CGFloat = 8
 
     override init(frame: CGRect) {
@@ -26,26 +24,5 @@ class Overlay: UIView {
 
     private func setup() {
         self.clipsToBounds = false
-    }
-
-    override func layoutSubviews() {
-        let path = CGMutablePath()
-
-        _mask.frame = self.bounds
-
-        var clipping = self.bounds
-        clipping.origin.y = self.bounds.size.height - cornerRadius
-
-        path.addRect(self.bounds)
-        path.addRoundedRect(in: clipping, cornerWidth: cornerRadius, cornerHeight: cornerRadius)
-
-        _mask.path = path
-        #if swift(>=4.2)
-        _mask.fillRule = .evenOdd
-        #else
-        _mask.fillRule = kCAFillRuleEvenOdd
-        #endif
-
-        self.layer.mask = _mask
     }
 }
